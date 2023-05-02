209dfa18832badc469c7fdde4abf0962fc501da7
Leanne Gass <leannegass@gmail.com>
2023-05-02 15:21:09 +0100

    innitial commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+const fetch = require('isomorphic-fetch');
+
+//const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+async function getImage(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+getImage("cats");
