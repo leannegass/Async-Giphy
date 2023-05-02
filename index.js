afe4a3d26982df29a8b8554cb25dfde7e8d6c18f
Leanne Gass <leannegass@gmail.com>
2023-05-02 15:48:06 +0100

    get first url from json object

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const fetch = require('isomorphic-fetch');
 
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
 
 async function getImage(query){
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
 
     const response = await fetch(endpoint);
     const data = await response.json();
-    console.log(data);
+    //const newd = await JSON.parse(data.data)
+    const firstURL = data.data[0].url
+    
+    console.log(firstURL);
     return data;
 }
 getImage("cats");
